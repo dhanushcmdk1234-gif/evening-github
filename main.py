230a754b5dd72fea2b347f60724308e3a5258730
Dhanush <dhanush.cmdk.1234@gmai.com>
2025-09-23 22:41:02 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,7 @@
+#input the name from users
+name=str(input("enter your name: "))
+age=int(input("enter your age: "))
+gender=str(input("enter your gender: "))
+
+#display to the user
+print(f"hello, {name} your age is {age} and your gender is {gender}")
