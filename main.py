d4b10634b19f95b1d9a2158aafd8ad4522bed16b
dhanushcmdk1234-gif <dhanush.cmdk.1234@gmail.com>
2025-09-24 20:55:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ age=int(input("enter your age: "))
 gender=str(input("enter your gender: "))
 
 #display to the user
-print(f"hello, {name} your age is {age} and your gender is {gender}")
+print(f"hello welcome {name} your age is {age} and your gender is {gender} all the best")
